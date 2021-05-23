47ea6637f4c1e09d924fcf16d16687c17ac80222
Konstantin Yurchenko Jr <konstantin.k.yurchenko@gmail.com>
2021-05-23 14:40:51 -0700

    Fixes inspect script.

--- src/server/controllers/bash/inspect.sh
+++ src/server/controllers/bash/inspect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PID="$(ps -A | grep masters-of | awk '{print $1}')" && echo "${PID}"
-ps -p $PID -o %cpu,%mem,cmd
+echo "CPU `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'` HDD `df -h / | awk '/\// {print $(NF-1)}'`"
+
