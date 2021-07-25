9188f8df4ad0cc05cdb4a4bcfda51476983dd148
Konstantin Yurchenko Jr <konstantin.k.yurchenko@gmail.com>
2021-07-24 17:39:59 -0700

    Fixes permissions.

--- src/server/server.ts
+++ src/server/server.ts
@@ -25,14 +25,6 @@ app.use('/api', apiRouter);
 app.use('/forms', formsRouter);
 
 
-app.use('/files/masters-of-conquest-headless_Data/StreamingAssets/code.txt', (req: any, res: any, next: any) => {
-    return res.status(403).end('403 Forbidden')
-})
-
-app.use('/files/masters-of-conquest-headless_Data/StreamingAssets/pin.txt', (req: any, res: any, next: any) => {
-    return res.status(403).end('403 Forbidden')
-})
-
 app.use(
     "/files",
     express.static("masters-of-conquest-headless"),
@@ -43,6 +35,13 @@ app.get('*', function (req: any, res: any) {
     res.sendFile('index.html', { root: path.join(__dirname, process.env.PATH_PUBLIC) });
 });
 
+app.get('/files/masters-of-conquest-headless_Data/StreamingAssets/code.txt', (req: any, res: any, next: any) => {
+    return res.status(403).end('403 Forbidden')
+})
+
+app.get('/files/masters-of-conquest-headless_Data/StreamingAssets/pin.txt', (req: any, res: any, next: any) => {
+    return res.status(403).end('403 Forbidden')
+})
 
 ///////////////////////////////////////////////////////////////////////////////
 // SOCKET
