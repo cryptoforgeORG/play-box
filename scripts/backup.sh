08c95e347d34a5aee80efb669d19d684fd890715
Konstantin Yurchenko Jr <konstantin.k.yurchenko@gmail.com>
2021-12-04 19:26:08 -0800

    Fixes utility.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,29 +4,29 @@
 URL=ubuntu@box-1.cryptoforge.org
 
 PS3='Please enter your choice: '
-options=("push_backup_1" "push_backup_2" "pull_backup_1" "pull_backup_2" "quit")
+options=("tar_import" "tar_export" "push" "pull" "quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "push_backup_1")
+        "tar_import")
+            cmd="tar -cvzf ./backups/import.tar.gz ./backups/.lnd"
+            echo $cmd
+            $cmd    
+            ;;
+
+        "tar_export")
+            cmd="tar -xzvf import.tar.gz"
+            echo $cmd
+            $cmd    
+            ;;
+
+        "push")
             cmd="scp -i ~/.ssh/box -r ./backups/import.tar.gz $URL:/box/import.tar.gz"
             echo $cmd
             $cmd            
             ;;
 
-        "push_backup_2")
-            cmd="scp -i ~/.ssh/box -r ./backups/import.tar.gz $URL:/box/import.tar.gz"
-            echo $cmd
-            $cmd            
-            ;;
-
-        "pull_backup_1")
-            cmd="scp -i ~/.ssh/box -r $URL:/box/export.tar.gz ."
-            echo $cmd
-            $cmd            
-            ;;
-
-        "pull_backup_2")
+        "pull")
             cmd="scp -i ~/.ssh/box -r $URL:/box/export.tar.gz ."
             echo $cmd
             $cmd            
